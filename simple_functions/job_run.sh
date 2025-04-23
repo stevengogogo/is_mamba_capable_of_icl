e47152bb15e7c787cf1b404d14c14563d1c2aba9
Steven Chiu <stchiu@tamu.edu>
2025-04-23 12:09:33 -0500

    use one gpu

--- simple_functions/job_run.sh
+++ simple_functions/job_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=in_context_learning
+#SBATCH --job-name=in_context_learning_mamba
 #SBATCH --time=0-00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --output=logs/out.%j-%x-%2t
-#SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:a100:1
 #SBATCH --partition=gpu
 #SBATCH --mail-type=END
 #SBATCH --mail-user=stchiu@tamu.edu
@@ -16,6 +16,7 @@ module load GCC/12.3.0  OpenMPI/4.1.5 #PyTorch/2.1.2-CUDA-12.1.1
 
 module load Anaconda3/2024.02-1
 source activate in-context-learning-mamba
+nvidia-smi
 which python
 #cp -r ./data $TMPDIR
 
